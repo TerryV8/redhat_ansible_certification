2d56ec6116b75a7160f20b356bfe6356e14b42a3
TerryV8 <thierrylam.vo@gmail.com>
2018-12-20 21:14:41 +0100

    Update 2.sh

--- 13_ansible_tower/2.sh
+++ 13_ansible_tower/2.sh
@@ -1,3 +1,19 @@
+
+ssh root@tower
+
 wget http://.../ansible-tower-setup-bundle-3.tar.gz
+
 tar xfz ansible-tower-setup-bundle-3.tar.gz
+
+Edit inventory:
+admin_password = 'redhat'
+pg_password = 'redhat'
+rabbitmq_password = 'redhat'
+
+./setup.sh
+
+exit
+
+
+https://tower.lab.example.com
 
